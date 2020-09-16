e7a5ed98402acbabdd7fa10a79e3d18960e9bc17
Rusydy <rusydymuhiddin@gmail.com>
2020-09-16 16:20:44 +0700

    finally done

--- 3/main.go
+++ 3/main.go
@@ -1,5 +1,23 @@
 package main
 
-func maxRot(n int64) int64 {
-	// your code
+import (
+	"fmt"
+	"strconv"
+)
+
+func MaxRot(n int64) int64 {
+	max, rot := n, strconv.FormatInt(n, 10)
+	for i := 0; i < len(rot); i++ {
+		rot = rot[:i] + rot[i+1:] + rot[i:i+1]
+		num, _ := strconv.ParseInt(rot, 10, 64)
+		if num > max {
+			max = num
+		}
+		fmt.Println(num)
+	}
+	return max
+}
+
+func main() {
+	fmt.Println(MaxRot(38458215)) //85821534
 }
