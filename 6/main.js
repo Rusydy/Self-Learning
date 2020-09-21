3656dbec291fdd45f773770798355e15ea54c15e
Rusydy <rusydymuhiddin@gmail.com>
2020-09-22 00:31:21 +0700

    DONE!

---
+++ 6/main.js
@@ -0,0 +1,47 @@
+/*
+Original  
+    String.prototype.toJadenCase = function () {
+    //...
+    };
+*/
+
+/*
+PSEUDOCODE
+
+    Set up a place to store the jadenCaseStr
+    Set up a palce to store wether the next letter we encounter shuld be capitalized
+
+    Always capitalize the first letter of the string
+    Iterate over each letter in the string
+        if... the current letter is a space, next letter should be capitalized
+        else... append either the letter itself, or capitalized letter
+*/
+
+String.prototype.toJadenCase = function () {
+    let JadenCase = ''
+    let capitalize = true
+
+    for (let i = 0; i < this.length; i++) {
+        const letter = this[i]
+
+        if (letter === ' ') {
+            capitalize = true
+            JadenCase += ' '
+        } else {
+            if (capitalize) {
+                JadenCase += letter.toUpperCase()
+                capitalize = false
+            } else {
+                JadenCase += letter.toLowerCase()
+            }
+        }
+    }
+
+    return JadenCase
+};
+
+
+let str = "How can mirrors be real if our eyes aren't real"; 
+
+let JadenCase = str.toJadenCase();
+console.log(JadenCase)
